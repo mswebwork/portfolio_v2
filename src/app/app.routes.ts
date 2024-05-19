5a1133551e0098c382e719cc4c0ed1729012f2a3
INQO <mszyszka.web@gmail.com>
2024-05-19 21:20:38 +0200

    add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import {HomepageComponent} from "./pages/homepage/homepage.component";
+import {PageNotFoundComponent} from "./pages/page-not-found/page-not-found.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomepageComponent },
+  
+  { path: '**', component: PageNotFoundComponent}
+];
