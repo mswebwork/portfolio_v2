207a7523fdfcfcc134b92493d95288578f7655e6
INQO <mszyszka.web@gmail.com>
2024-05-19 21:22:26 +0200

    create empty components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,6 @@ import {PageNotFoundComponent} from "./pages/page-not-found/page-not-found.compo
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'home', component: HomepageComponent },
-  
+
   { path: '**', component: PageNotFoundComponent}
 ];
